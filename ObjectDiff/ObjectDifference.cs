a5368e516863c7ab0a3244163f84b51d78855ca4
Alexander Lowe <aplowe@gmail.com>
2014-05-23 02:14:15 -0700

    Initial big commit

---
+++ ObjectDiff/ObjectDifference.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.XmlDiffPatch;
+using System.IO;
+
+namespace ObjectDiff
+{
+    public class ObjectDifference
+    {
+
+        public string Difference(object original, object changed)
+        {
+            XmlReader originalreader = XmlReader.Create(new StringReader(Serialize(original)));
+            XmlReader changedreader = XmlReader.Create(new StringReader(Serialize(changed)));
+
+            StringBuilder patch = new StringBuilder();
+            XmlWriter writer = XmlWriter.Create(patch);
+
+            XmlDiff xmldiff = new XmlDiff(XmlDiffOptions.IgnoreChildOrder |
+                                             XmlDiffOptions.IgnoreNamespaces |
+                                             XmlDiffOptions.IgnorePrefixes);
+
+            bool bIdentical = xmldiff.Compare(originalreader, changedreader, writer);
+            writer.Close();
+
+            return patch.ToString();
+        }
+
+
+        // public void PatchUp(string originalFile, String diffGramFile, String OutputFile)
+        public object Patch(object source, string patch)
+        {
+            XmlReader readersource = XmlReader.Create(new StringReader(Serialize(source)));
+            
+
+            XmlDocument docsource = new XmlDocument(new NameTable());
+            docsource.Load(readersource);
+            XmlTextReader readerpatch = new XmlTextReader(new StringReader(patch));
+
+            XmlPatch xmlpatch = new XmlPatch();
+
+            xmlpatch.Patch(docsource, readerpatch);
+
+            StringBuilder newxml = new StringBuilder();
+            XmlWriter writernewxml = XmlWriter.Create(newxml);
+
+            docsource.Save(writernewxml);
+
+            return Deserialize(newxml.ToString(), source.GetType());
+        }
+
+
+
+        private string Serialize(object o)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            // Serialization
+            XmlSerializer s = new XmlSerializer(o.GetType());
+            using (StringWriter w = new StringWriter(buffer))
+            {
+                s.Serialize(w, o);
+            }
+
+            return buffer.ToString();
+
+        }
+
+        private object Deserialize(string xml, Type type)
+        {
+            XmlSerializer s = new XmlSerializer(type);
+            using (StringReader r = new StringReader(xml))
+            {
+                object o = s.Deserialize(r);
+                return o;
+            }
+        }
+
+    }
+}
---
+++ ObjectDiff/Tests/DiffTests.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ObjectDiff.Tests
+{
+    [TestFixture]
+    public class DiffTests
+    {
+        [Test]
+        public void NoChange()
+        {
+            ObjectDifference diff = new ObjectDifference();
+
+            A a1 = new A();
+
+            a1.i1 = 1;
+            a1.s1 = "Hello World";
+            a1.a2.d1 = 3.14;
+            a1.a2.s1 = "Foo";
+            a1.a2.s2 = "Bar";
+
+
+
+            A a2 = new A();
+
+            a2.i1 = 1;
+            a2.s1 = "Hello World";
+            a2.a2.d1 = 3.14;
+            a2.a2.s1 = "Foo";
+            a2.a2.s2 = "Bar";
+
+
+
+            string patch = diff.Difference(a1, a2);
+
+
+            A anew = diff.Patch(a1, patch) as A;
+
+            Assert.AreEqual(a2.i1, anew.i1);
+        }
+
+        [Test]
+        public void ChangeLevel1()
+        {
+            ObjectDifference diff = new ObjectDifference();
+
+            A a1 = new A();
+
+            a1.i1 = 1;
+            a1.s1 = "Hello World";
+            a1.a2.d1 = 3.14;
+            a1.a2.s1 = "Foo";
+            a1.a2.s2 = "Bar";
+
+
+
+            A a2 = new A();
+
+            a2.i1 = 2;
+            a2.s1 = "Hello World";
+            a2.a2.d1 = 3.14;
+            a2.a2.s1 = "Foo";
+            a2.a2.s2 = "Bar";
+
+
+
+            string patch = diff.Difference(a1, a2);
+
+
+            A anew = diff.Patch(a1, patch) as A;
+
+            Assert.AreEqual(anew.i1, 2);
+        }
+
+        [Test]
+        public void ChangeLevel2()
+        {
+            ObjectDifference diff = new ObjectDifference();
+
+            A a1 = new A();
+
+            a1.i1 = 1;
+            a1.s1 = "Hello World";
+            a1.a2.d1 = 3.14;
+            a1.a2.s1 = "Foo";
+            a1.a2.s2 = "Bar";
+
+
+
+            A a2 = new A();
+
+            a2.i1 = 1;
+            a2.s1 = "Hello World";
+            a2.a2.d1 = 1.23;
+            a2.a2.s1 = "Foo";
+            a2.a2.s2 = "Bar";
+
+
+
+            string patch = diff.Difference(a1, a2);
+
+
+            A anew = diff.Patch(a1, patch) as A;
+
+            Assert.AreEqual(anew.a2.d1, 1.23);
+        }
+
+        [Test]
+        public void ListChange()
+        {
+            ObjectDifference diff = new ObjectDifference();
+
+            A a1 = new A();
+
+            a1.i1 = 1;
+            a1.s1 = "Hello World";
+            a1.a2.d1 = 3.14;
+            a1.a2.s1 = "Foo";
+            a1.a2.s2 = "Bar";
+            a1.stringlist.Add("Hello");
+            a1.a2list.Add(new A2() { d1=123 });
+
+            A a2 = new A();
+
+            a2.i1 = 1;
+            a2.s1 = "Hello World";
+            a2.a2.d1 = 3.14;
+            a2.a2.s1 = "Foo";
+            a2.a2.s2 = "Bar";
+            a2.stringlist.Add("World");
+            a2.a2list.Add(new A2() { d1 = 456 });
+
+
+
+            string patch = diff.Difference(a1, a2);
+
+
+            A anew = diff.Patch(a1, patch) as A;
+
+            Assert.AreEqual(anew.stringlist.Exists(a=>a=="Hello"), false);
+            Assert.AreEqual(anew.stringlist.Exists(a => a == "World"), true);
+
+
+            Assert.AreEqual(anew.a2list.Exists(a => a.d1==123), false);
+            Assert.AreEqual(anew.a2list.Exists(a => a.d1 == 456), true);
+        }
+    }
+
+
+
+    public class A
+    {
+        public int i1;
+        public string s1;
+
+        public A2 a2 = new A2();
+
+        public List<string> stringlist = new List<string>();
+
+        public List<A2> a2list = new List<A2>();
+    }
+
+    public class A2
+    {
+        public double d1;
+        public string s1;
+        public string s2;
+    }
+}
